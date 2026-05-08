497f1bc35d506ddcde260bb16708c550893c2927
Subodh668 <smsb668@gmail.com>
2026-05-08 19:41:38 +0545

    Added: mob scene, Updated:main scene

---
+++ scripts/mob.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+#main player
+@onready var player = get_node("/root/Game/Player")
+
+#func _ready():
+#	player = get_node("/root/Game/Player")
+
+func _physics_process(delta):
+	var direction = global_position.direction_to(player.global_position) #the current position of the character in the game
+	velocity = direction * 300.0
+	move_and_slide()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
 func _physics_process(delta): 
+	#get user vectors of user input or the key pressed
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	#override the velocity of the character when key pressed
 	velocity = direction * 600
+	#function to move and slide to obstacle based on the velocity and the keys pressed
 	move_and_slide()
 	
+	#when key pressed the direction vector increases so the velocity and trigger animations
 	if velocity.length() > 0.0: 
-		%HappyBoo.play_walk_animation()
+		%HappyBoo.play_walk_animation() # % indicates the custom node 
 	else: 
 		%HappyBoo.play_idle_animation()
