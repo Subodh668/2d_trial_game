34ac1bc06381b8fc3f5290bdea89f86d50c7b401
Subodh668 <smsb668@gmail.com>
2026-05-09 16:49:55 +0545

    Refactor game scenes and scripts: update player and survivor game scenes, adjust editor configurations, and enhance mob behavior

--- scripts/mob.gd
+++ scripts/mob.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-#main player
+#track main player when game run the instance is created
 @onready var player = get_node("/root/Game/Player")
 
 #func _ready():
@@ -10,3 +10,4 @@ func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position) #the current position of the character in the game
 	velocity = direction * 300.0
 	move_and_slide()
+  
